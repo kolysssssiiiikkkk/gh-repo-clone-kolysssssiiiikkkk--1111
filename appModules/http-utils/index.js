b568d2bcb10da8cb5965e3093ab98c92923d4dfe
Ryabov Nikolay <149957317+kolysssssiiiikkkk@users.noreply.github.com>
2024-05-24 22:19:46 +0500

    hshshswhhfhwh

--- appModules/http-utils/index.js
+++
@@ -1,9 +0,0 @@
-const parseBody = require("./parse-body");
-const mimeTypes = require("./mime-types");
-const staticFile = require("./static-file");
-
-module.exports = {
-    staticFile,
-    mimeTypes,
-    parseBody
-}
